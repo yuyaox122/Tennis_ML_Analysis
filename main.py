30f989f15754aca1d3567c6fe2d2507a46ff7222
yuyaox122 <yuyaoxie122@gmail.com>
2024-10-03 19:17:49 +0100

    Added mini court

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from utilities import (read_video, 
+from utilities import (read_video,
                        save_video)
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 import cv2
 
 def main():
-    input_video_path = "EPQ_Tennis/input_videos/short_clip.mp4"
+    input_video_path = "input_videos/short_clip.mp4"
     # Read the video frames
     video_frames = read_video(input_video_path)
 
@@ -14,26 +15,32 @@ def main():
     ball_tracker = BallTracker(model_path="C:/Users/yuyao/OneDrive/Documents/EPQ/models/yolov5_last.pt")
     player_detections = player_tracker.detect_frames(video_frames,
                                                     read_from_stub=True,
-                                                    stub_path="EPQ_Tennis/tracker_stubs/player_detections.pkl")
+                                                    stub_path="tracker_stubs/player_detections.pkl")
     ball_detections = ball_tracker.detect_frames(video_frames,
                                                     read_from_stub=True,
-                                                    stub_path="EPQ_Tennis/tracker_stubs/ball_detections.pkl")
+                                                    stub_path="tracker_stubs/ball_detections.pkl")
     
 
     court_model_path = "C:/Users/yuyao/OneDrive/Documents/EPQ/models/court_points_model.pth"
     court_line_detector = CourtLineDetector(court_model_path)
     court_keypoints = court_line_detector.predict(video_frames[0])
     player_detections = player_tracker.filter_players(court_keypoints, player_detections)
+    mini_court = MiniCourt(video_frames[0])
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
     # Draw bounding boxes around the players and balls
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
+    
+    # Draw the key points on the court
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
-    save_video(output_video_frames, "EPQ_Tennis/output_videos/output_video.avi")
-
+    
+    # Draw the mini court  
+    output_video_frames = mini_court.draw_mini_court(output_video_frames)
+    
     # Add frame numbers to the output video
     for i, frame in enumerate(output_video_frames):
         cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 
 if __name__ == "__main__":
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,9 +1,3 @@
 from .video_utilities import read_video, save_video
-from .bbox_utils import get_centre_of_bbox, measure_distance, get_foot_position, get_closest_keypoint_index, get_height_of_bbox, measure_xy_distance, get_centre_of_bbox
-# def main():
-#     input_path = "input_videos/input_video.mp4"
-#     video_frames = read_video(input_path)
-#     save_video(video_frames, "output_videos/output_video.avi")
-
-# if __name__ == "__main__":
-#     main()
+from .bbox_utils import get_centre_of_bbox, measure_distance, get_foot_position, get_closest_keypoint_index, get_height_of_bbox, measure_xy_distance
+from .conversion_utils import convert_meters_to_pixel_distance, convert_pixel_distance_to_meters
